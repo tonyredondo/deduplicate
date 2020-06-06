17a54eb9b1f9ec22aa460c22365f644a57981bf0
Daniel Redondo <tonyredondo@gmail.com>
2020-06-06 23:25:28 +0200

    Concurrency version using goroutines (#2)
    * parallel in hash calculations
    
    * concurrency fix
    
    * parallel copy and concurrency level increase
    
    * fix waitgroup deadlock
    
    * fixes and summary info
    
    * changes

--- cmd/root.go
+++ cmd/root.go
@@ -8,7 +8,10 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -19,6 +22,10 @@ import (
 )
 
 const timeFormat = "20060102T150405"
+
+type (
+	workerJob func()
+)
 
 var (
 	extensions = map[string]interface{}{
@@ -44,7 +51,11 @@ var (
 		".m2v":  nil,
 		".mp2":  nil,
 	}
-	hashes map[string]string
+	hashesMutex  = sync.Mutex{}
+	hashes       map[string]string
+	collisions   int64
+	copyErrors   int64
+	removeErrors int64
 
 	sources     []string
 	destination string
@@ -57,6 +68,8 @@ var (
 		Long:  `Deduplicate is an utility to remove duplicate images and rename the unique ones`,
 		Run:   run,
 	}
+
+	workerJobs chan workerJob
 )
 
 func init() {
@@ -75,6 +88,7 @@ func Execute() {
 }
 
 func run(cmd *cobra.Command, args []string) {
+	start := time.Now()
 	var folders []string
 	var dest string
 	for _, item := range sources {
@@ -96,18 +110,45 @@ func run(cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 
+	// start workers
+	concurrency := runtime.NumCPU() * 2
+	workerJobs = make(chan workerJob, concurrency)
+	for i := 0; i < concurrency; i++ {
+		go worker(i + 1)
+	}
+	atomic.StoreInt64(&collisions, 0)
+	atomic.StoreInt64(&copyErrors, 0)
+	atomic.StoreInt64(&removeErrors, 0)
+
 	fmt.Println("Source Folders:", folders)
 	fmt.Println("Destination:", dest)
 	fmt.Println("Rename:", rename)
+	fmt.Println("Move:", move)
+	fmt.Println("Concurrency Level:", concurrency)
 	fmt.Println()
 
+	fmt.Println("Calculating hashes...")
 	populateHash(folders)
+	fmt.Println()
+	fmt.Println("Processing hashes...")
 	processHashes(dest)
+
+	close(workerJobs)
+
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("Total number of images with no duplicates:", len(hashes))
+	fmt.Println("Total number of duplicates:", atomic.LoadInt64(&collisions))
+	fmt.Println("Total number of copy errors:", atomic.LoadInt64(&copyErrors))
+	if move {
+		fmt.Println("Total number of remove errors:", atomic.LoadInt64(&removeErrors))
+	}
+	fmt.Printf("Done in %v", time.Since(start))
 }
 
 func populateHash(folders []string) {
 	hashes = map[string]string{}
-	collisions := 0
+	wg := sync.WaitGroup{}
 	for _, item := range folders {
 		files, err := ioutil.ReadDir(item)
 		if err != nil {
@@ -118,76 +159,100 @@ func populateHash(folders []string) {
 				continue
 			}
 			if _, ok := extensions[strings.ToLower(filepath.Ext(f.Name()))]; ok {
-				fPath := filepath.Join(item, f.Name())
-
-				data, err := ioutil.ReadFile(fPath)
-				if err != nil {
-					fmt.Println("Error:", fPath, err)
+				wg.Add(1)
+				fPath := item
+				fName := f.Name()
+				job := func() {
+					processFileHash(&wg, fPath, fName, &collisions)
 				}
-				hash := sha512.Sum512(data)
-				strHash := fmt.Sprintf("%x", hash)
-
-				if current, ok := hashes[strHash]; ok {
-					collisions++
-					fmt.Printf("(%d). File '%s' duplicate with: '%s'. Ignoring it. \n", collisions, fPath, current)
-				} else {
-					hashes[strHash] = fPath
-				}
+				workerJobs <- job
 			}
 		}
 	}
+
+	wg.Wait()
 	fmt.Println()
 	fmt.Println("Total number of images:", len(hashes))
 	fmt.Println("Total number of duplicates:", collisions)
 }
 
+func processFileHash(wGroup *sync.WaitGroup, path string, fName string, collisionsCounter *int64) {
+	defer wGroup.Done()
+	fPath := filepath.Join(path, fName)
+
+	data, err := ioutil.ReadFile(fPath)
+	if err != nil {
+		fmt.Println("Error:", fPath, err)
+	}
+	hash := sha512.Sum512(data)
+	strHash := fmt.Sprintf("%x", hash)
+
+	hashesMutex.Lock()
+	defer hashesMutex.Unlock()
+	if current, ok := hashes[strHash]; ok {
+		currentCollisions := atomic.AddInt64(collisionsCounter, 1)
+		fmt.Printf("(%d) File '%s' duplicate with: '%s'. Ignoring it. \n", currentCollisions, fPath, current)
+	} else {
+		hashes[strHash] = fPath
+	}
+}
+
 func processHashes(dest string) {
 	fmt.Println()
+	wg := sync.WaitGroup{}
+
 	for _, v := range hashes {
-		var destFileName string
+		wg.Add(1)
+		sourceFile := v
+		workerJobs <- func() {
+			defer wg.Done()
+			var destFileName string
 
-		if rename {
-			fTime, err, noRename := getFileTime(v)
-			if err != nil {
-				fTime = time.Now()
-				fmt.Println(err)
-			}
-			if noRename {
-				destFileName = filepath.Base(v)
-			} else {
-				destFileName = fmt.Sprintf("%s %s", fTime.Format(timeFormat), filepath.Base(v))
-				destFileName = strings.ReplaceAll(destFileName, ":", "")
-			}
-		} else {
-			destFileName = filepath.Base(v)
-		}
-
-		nf := filepath.Clean(filepath.Join(dest, destFileName))
-
-		if v == nf {
-			fmt.Printf("Skipped: source '%s' is the same as '%s'\n", v, nf)
-		} else {
-			if simulate {
-				fmt.Println(v, "->", nf)
-			} else {
-				if !move {
-					fmt.Printf("Copying '%s' to '%s'\n", v, nf)
-				} else {
-					fmt.Printf("Moving '%s' to '%s'\n", v, nf)
-				}
-				err := copyFile(v, nf)
+			if rename {
+				fTime, err, noRename := getFileTime(sourceFile)
 				if err != nil {
-					fmt.Println("Error: copying file:", err)
-					fmt.Println()
-				} else if move {
-					if err := os.Remove(v); err != nil {
-						fmt.Println("Error: removing source file:", err)
-						fmt.Println()
+					fTime = time.Now()
+					fmt.Println(err)
+				}
+				if noRename {
+					destFileName = filepath.Base(sourceFile)
+				} else {
+					destFileName = fmt.Sprintf("%s %s", fTime.Format(timeFormat), filepath.Base(sourceFile))
+					destFileName = strings.ReplaceAll(destFileName, ":", "")
+				}
+			} else {
+				destFileName = filepath.Base(sourceFile)
+			}
+
+			destinationFile := filepath.Clean(filepath.Join(dest, destFileName))
+
+			if sourceFile == destinationFile {
+				fmt.Printf("Skipped: source '%s' is the same as '%s'\n", sourceFile, destinationFile)
+			} else {
+				if simulate {
+					fmt.Println(sourceFile, "->", destinationFile)
+				} else {
+					if !move {
+						fmt.Printf("Copying '%s' to '%s'\n", sourceFile, destinationFile)
+					} else {
+						fmt.Printf("Moving '%s' to '%s'\n", sourceFile, destinationFile)
+					}
+					err := copyFile(sourceFile, destinationFile)
+					if err != nil {
+						fmt.Printf("Error: copying file '%s': %v\n", sourceFile, err)
+						atomic.AddInt64(&copyErrors, 1)
+					} else if move {
+						if err := os.Remove(sourceFile); err != nil {
+							fmt.Printf("Error: removing source file '%s': %v", sourceFile, err)
+							atomic.AddInt64(&removeErrors, 1)
+						}
 					}
 				}
 			}
 		}
 	}
+
+	wg.Wait()
 }
 
 func getFileTime(filePath string) (fileTime time.Time, err error, timeInPath bool) {
@@ -282,4 +347,16 @@ func copyFileContents(src, dst string) (err error) {
 	}
 	err = out.Sync()
 	return
+}
+
+func worker(id int) {
+	for {
+		select {
+		case job, ok := <-workerJobs:
+			if !ok {
+				return
+			}
+			job()
+		}
+	}
 }
